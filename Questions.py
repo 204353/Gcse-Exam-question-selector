3053fb893d048dd79bcc59cae8d054506d3a74db
204353 <69662795+204353@users.noreply.github.com>
2020-08-15 15:04:28 +0800

    Add files via upload

---
+++ Questions.py
@@ -0,0 +1,4 @@
+import random
+command = ["Analyse", "Define", "Discuss", "Explain", "Outline"]
+Question = ["Abstraction", "Iteration", "Selection", "Stepwise Refinement", "Flowcharts"]
+print(random.choice(command), random.choice(Question))
